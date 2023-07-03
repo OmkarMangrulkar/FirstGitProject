0a744682f032385de1f5632c7ef3f6d2dace411c
ommangrulkar <ommangrulkar4445@gmail>
2023-07-04 01:49:52 +0530

    Add comment in code

--- Future Sales Prediction.py
+++ Future Sales Prediction.py
@@ -5,9 +5,14 @@ data = pd.read_csv(r"C:\Users\omkar mangrulkar\Downloads\archive (7)\Advertising
 
 data.info()
 
+#-- Let’s have a look at whether this dataset contains any null values or not:
+
 data.isnull().sum()
 
 data = data.drop('Unnamed: 0',axis=1)
+
+#-- So this dataset doesn’t have any null values. Now let’s visualize the relationship 
+#   between the amount spent on advertising on TV and units sold:
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -30,6 +35,10 @@ plt.show()
 correlation = data.corr()
 print(correlation['Sales'].sort_values(ascending=False))
 
+#-- Out of all the amount spent on advertising on various platforms,
+#   I can see that the amount spent on advertising the product on TV results
+#   in more sales of the product. Now let’s have a look at the correlation of
+#   all the columns with the sales column:
 
 x = np.array(data.drop(["Sales"], 1))
 y = np.array(data["Sales"])
